6448226e95f525ace143435ffffc66c86e4a9930
victorcid <vcfiguera@gmail.com>
2023-08-12 11:40:35 +0300

    ddl exercise 3 solution

--- db_connector/sqlite3/exercises/ddl.py
+++ db_connector/sqlite3/exercises/ddl.py
@@ -1,25 +1,66 @@
+import sqlite3
+
+conn = sqlite3.connect("movies.sqlite")
+cursor = conn.cursor()
+
+
+# 1. Create a Database and Tables
+cursor.execute(
+    """
+CREATE TABLE authors(
+            author_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+)
 """
-Exercise 3
+)
 
-Objective: Create a database for managing a library, which includes tables 
-for books and authors. Perform specific tasks to manipulate these tables.
-
-TASKS:
-
-1. Create a Database and Tables
-    - Create a table name `authors` within the following columns:
-    * `author_id`: Integer, Primary Key, Autoincrement
-    * `name`: Text, Not NULL
-
-    - Create a table named `books` with the following columns:
-    * `book_id`: Integer, Primary Key, Autoincrement
-    * title: Text, Not NULL
-    * `author_id`: Integer, Foreign Key referencing authors.author_id
-    * `published_date`: Text
-
-2. Alter the Schema
-    * Add columns genre:text and publisher:text and ISBM: integer
-
-3. Delete a Table
-    * Delete the `authors` table.
+cursor.execute(
+    """
+CREATE TABLE books(
+            book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            author_id INTEGER,
+            published_date TEXT,
+            FOREIGN KEY (author_id) REFERENCES authors(author_id)
+)
 """
+)
+
+# 2. Alter the Schema
+cursor.execute(
+    """
+ALTER TABLE books ADD COLUMN genre TEXT
+"""
+)
+
+cursor.execute(
+    """
+ALTER TABLE books ADD COLUMN publisher TEXT
+"""
+)
+
+cursor.execute(
+    """
+ALTER TABLE books ADD COLUMN ISBM INTEGER
+"""
+)
+
+conn.commit()
+conn.close()
+
+
+authors_drop = input("You want to drop the authors table? <yes/no>")
+if authors_drop == "yes":
+    # 3. Delete a Table
+    conn = sqlite3.connect("movies.sqlite")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        DROP TABLE authors
+        """
+    )
+    conn.commit()
+    conn.close()
+else:
+    print("Sure I will not delete it :) ")
